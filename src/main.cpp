d094c3726701abf22400ddda03635620fd80eeed
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-08-20 11:17:29 +0100

    Sam: Serial prints included for monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
+#include <string>
 
 #include "ground.cpp"
 
 Ground ground;
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+
   ground = Ground();
+  Serial.println("Setting up ground");
   ground.setup();
+  Serial.println("Ground setup");
 }
 
 void loop() {
-  ground.receiveData();
+  String message = ground.receiveData();
+  Serial.println(message);
 }
